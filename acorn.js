ecb0b23d9d2aeadb34e2ec1d6db4fb685e084bb5
Karl Böhlmark <karl.bohlmark@gmail.com>
2013-08-09 17:06:56 +0200

    ensure works with component builder

--- acorn.js
+++ acorn.js
@@ -21,7 +21,7 @@
 // [walk]: util/walk.js
 
 (function(root, mod) {
-  if (typeof exports == "object" && typeof module == "object") return mod(exports); // CommonJS
+  if (typeof exports == "object" && typeof module != "undefined") return mod(exports); // CommonJS
   if (typeof define == "function" && define.amd) return define(["exports"], mod); // AMD
   mod(root.acorn || (root.acorn = {})); // Plain browser env
 })(this, function(exports) {
